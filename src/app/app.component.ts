604eb596b6742a728246d3da10139f1f0d72c6bc
volidenko <volidenko@.i.ua>
2020-12-03 23:52:58 +0200

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BillGates';
+  title='Bill Gates';
+  citation1 = 'Patience is a key element of success';
+  citation2 = 'If you think your teacher is tough, wait till you get a boss';
+  citation3 = 'Life is not fair — get used to it!';
+  citation4 = 'Success is a lousy teacher. It seduces smart people into thinking they can’t lose';
+  citation5 = 'Success is a lousy teacher. It seduces smart people into thinking they can’t lose';
 }
